facb00bddd8434935ebe0713eb8f326f02d6f803
João Santos <joao@talkagency.com.au>
2024-04-05 14:51:09 -0300

    simplified comments

--- index.js
+++ index.js
@@ -1,20 +1,9 @@
-/**
- * Express server for timestamp microservice.
- * @module index
- */
 const express = require("express");
 const app = express();
 const TimestampController = require("./controllers/TimestampController");
 app.use(express.json());
 
-/**
- * GET endpoint to retrieve timestamp information.
- * @name GET/api/:value
- * @function
- * @param {Object} req - Express request object.
- * @param {Object} res - Express response object.
- * @returns {Object} - JSON response containing timestamp information.
- */
+// GET endpoint to retrieve timestamp information.
 app.get("/api/:value", (req, res) => {
   const value = req.params.value;
   const timestampController = new TimestampController();
@@ -27,37 +16,18 @@ app.get("/api/:value", (req, res) => {
   }
 });
 
-/**
- * GET endpoint to serve an example HTML file.
- * @name GET/example
- * @function
- * @param {Object} req - Express request object.
- * @param {Object} res - Express response object.
- * @returns {Object} - HTML file as response.
- */
+// GET endpoint to serve an example HTML file.
 app.get("/example", function (req, res) {
   res.sendFile("./views/example.html", { root: __dirname });
 });
 
-/**
- * Default GET endpoint for handling undefined routes.
- * @name GET/*
- * @function
- * @param {Object} req - Express request object.
- * @param {Object} res - Express response object.
- * @returns {Object} - JSON response indicating resource not found.
- */
+// Default GET endpoint for handling undefined routes.
 app.get("*", function (req, res) {
   res.status(404).json({ error: "Resource not found" });
 });
 
-/**
- * Start the Express server on port 3000.
- * @name listen
- * @function
- * @param {number} port - Port number to listen on.
- * @param {Function} callback - Callback function to be executed when the server starts listening.
- */
+
+// Start the Express server on port 3000.
 app.listen(3000, () => {
   console.log("The app is running locally...");
 });
