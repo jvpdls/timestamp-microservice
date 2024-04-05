08514a293dfa374d90c1940ebfc5c3977ce53fe8
João Santos <joao@talkagency.com.au>
2024-04-05 15:13:35 -0300

    added robots.txt and updated console log

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
+const fs = require("fs");
+const path = require("path");
 const express = require("express");
 const app = express();
 const TimestampController = require("./controllers/TimestampController");
 app.use(express.json());
+
+// Gets the robots.txt file
+app.get("/robots.txt", function (req, res) {
+  res.setHeader("Content-Type", "text/plain");
+
+  const filePath = path.join(__dirname, "robots.txt");
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      res.statusCode = 200;
+      res.end("User-agent: *\nDisallow: /");
+    } else {
+      res.end(data);
+    }
+  });
+});
+
+// Redirect the homepage to the examples page
+app.get("/", (req, res) => {
+  res.redirect("/example");
+});
 
 // GET endpoint to retrieve timestamp information.
 app.get("/api/:value", (req, res) => {
@@ -26,8 +48,7 @@ app.get("*", function (req, res) {
   res.status(404).json({ error: "Resource not found" });
 });
 
-
 // Start the Express server on port 3000.
 app.listen(3000, () => {
-  console.log("The app is running locally...");
+  console.log("The app is running locally. Visit http://localhost:3000/example to start.");
 });
