de9f7a52162cd57a7eb7444f10b586c0f7040325
João Santos <joao@talkagency.com.au>
2024-04-05 15:48:08 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,15 @@
-const fs = require("fs");
 const path = require("path");
 const express = require("express");
 const app = express();
 const TimestampController = require("./controllers/TimestampController");
 app.use(express.json());
 
-// Gets the robots.txt file
-app.get("/robots.txt", function (req, res) {
-  res.setHeader("Content-Type", "text/plain");
-  res.sendFile("./robots.txt", { root: __dirname });
-});
+// Setting the static files directory
+app.use(express.static(path.join(__dirname, "public")));
 
 // Redirect the homepage to the examples page
-app.get("/", (req, res) => {
-  res.redirect("/example");
+app.get("/", (res) => {
+  res.redirect("/example.html");
 });
 
 // GET endpoint to retrieve timestamp information.
@@ -29,21 +25,16 @@ app.get("/api/:value", (req, res) => {
   }
 });
 
-// GET endpoint to serve an example HTML file.
-app.get("/example", function (req, res) {
-  res.sendFile("./views/example.html", { root: __dirname });
-});
-
 // Default GET endpoint for handling undefined routes.
-app.get("*", function (req, res) {
+app.get("*", function (res) {
   res.status(404).json({ error: "Resource not found" });
 });
 
-// Start the Express server on port 3000.
-app.listen(3000, () => {
-  console.log(
-    "The app is running locally. Visit http://localhost:3000/example to start."
-  );
-});
+// This was commented out to avoid conflicts when deplyoind on vercel
+// app.listen(3000, () => {
+//  console.log(
+//    "The app is running locally. Visit http://localhost:3000/example to start."
+//  );
+//});
 
 module.exports = app;
